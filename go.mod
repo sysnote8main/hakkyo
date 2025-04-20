46440b0bb8ebcabfd43f7631482b9a097c8d5cd7
sysnote8main <sysnote8main@gmail.com>
2025-04-20 21:32:34 +0900

    chore: initial commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/sysnote8main/hakkyo
+
+go 1.24.2
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.27.6 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/term v0.31.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/urfave/cli/v2"
+	"golang.org/x/term"
+)
+
+var (
+	HatoPatterns   []string = []string{"ぽ", "っ"}
+	NormalPatterns []string = []string{"お", "あ"}
+)
+
+func getRandomFromArray(arr []string, size int) string {
+	result := ""
+	for range size {
+		result += arr[rand.Intn(len(arr))]
+	}
+	return result
+}
+
+func main() {
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:  "hato",
+				Usage: "Set execution user as hato",
+			},
+			&cli.BoolFlag{
+				Name:  "force",
+				Usage: "Execute hakkyo forcefully",
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			isHato := ctx.Bool("hato")
+			isForce := ctx.Bool("force")
+			if isHato && !isForce {
+				fmt.Println("鳩を発狂させることはできません。")
+				os.Exit(1)
+			}
+
+			fmt.Println("hato", isHato, "force", isForce)
+			w, _, err := term.GetSize(int(os.Stdin.Fd()))
+			if err != nil {
+				slog.Debug("Failed to get terminal size. Fallback to default value (20)")
+				w = 20
+			}
+			w--
+			sig := make(chan os.Signal, 1)
+			signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
+		L:
+			for {
+				select {
+				case <-sig:
+					break L
+				default:
+					if isHato {
+						fmt.Println(getRandomFromArray(HatoPatterns, w))
+					} else {
+						fmt.Println(getRandomFromArray(NormalPatterns, w))
+					}
+				}
+			}
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		slog.Error("Failed to execute hakkyo", slog.Any("error", err))
+		os.Exit(1)
+	}
+}
